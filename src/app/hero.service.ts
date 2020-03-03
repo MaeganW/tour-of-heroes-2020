e0be0f1a307bc87ac128bba048684ae8d15c51c3
MaeganW <maeganwomble@gmail.com>
2020-03-03 10:26:41 -0600

    added synchronous hero service to get mock data - need to make async

---
+++ src/app/hero.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import {HEROES} from './mock-heroes';
+import {Hero} from './hero';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+  getHeros(): Hero[]{
+    return HEROES;
+  }
+
+  constructor() { }
+}
