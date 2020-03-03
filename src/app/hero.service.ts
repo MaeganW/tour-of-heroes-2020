db2411be67362d789124cd01b5027b814dbf8740
MaeganW <maeganwomble@gmail.com>
2020-03-03 13:49:49 -0600

    app now uses HttpClient to get heroes data

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,27 +1,55 @@
 import { Injectable } from '@angular/core';
-import { HEROES } from './mock-heroes';
 import { Hero } from './hero';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
-  // getHeros(): Hero[]{
+  private heroesUrl = 'api/heroes';  // url to web api
+  public hero: Hero;
+
+  // getHeros(): Hero[]{ // old method when we were getting data synchronously
   //   return HEROES;
   // }
 
-  getHeros(): Observable<Hero[]> {
-    this.messageService.addMessage('Hero Service: Fetched heroes');
-    return of(HEROES);
+  constructor(
+    private messageService: MessageService,
+    private http: HttpClient) {
+  }
+
+  getHeroes(): Observable<Hero[]> {
+    // return of(HEROES); // used this before we started using the HTTPCLIENT
+    return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        tap(_ => this.log('Fetched heroes')),
+        catchError(this.handleError<Hero[]>('getHeroes', []))
+      );
   }
 
   getHero(id: number): Observable<Hero> {
-    this.messageService.addMessage(`HeroService: Fetched hero id=${id}`);
-    return of(HEROES.find(hero => hero.id === id));
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`HeroService: Fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
   }
 
-  constructor(private messageService: MessageService) {
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error); // log to console instead
+      this.log(`${operation} failed: ${error.message}`);
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
+
+  private log(message: string) {
+    this.messageService.addMessage(`HeroService: ${message}`);
+  }
+
 }
